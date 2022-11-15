bb6b7cf42cb122ccc2eff417b37a4ba7bbbfd4bb
fishy52 <derfisch04@gmail.com>
2022-11-15 13:16:56 +0100

    Where do you live?

--- script.js
+++ script.js
@@ -2,3 +2,17 @@ function Search() {
     console.log(document.getElementById("search").value);
     window.open("https://www.google.com/search?q=" + document.getElementById("search").value + " David Korzeniowski");
 }
+
+
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  } else { 
+    x.innerHTML = "Geolocation is not supported by this browser.";
+  }
+}
+
+function showPosition(position) {
+    document.getElementById("demo").innerHTML = "Latitude: " + position.coords.latitude + 
+  "<br>Longitude: " + position.coords.longitude;
+}
